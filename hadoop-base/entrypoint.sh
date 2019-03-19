ee537843b268cd62a3a8975c52f5c74d9f71af72
Moa <honomoa@gmail.com>
2019-03-19 23:05:04 +0800

    Support configure mapred-site.xml

--- hadoop-base/entrypoint.sh
+++ hadoop-base/entrypoint.sh
@@ -37,6 +37,7 @@ configure $HADOOP_CONF_DIR/hdfs-site.xml hdfs HDFS_CONF
 configure $HADOOP_CONF_DIR/yarn-site.xml yarn YARN_CONF
 configure $HADOOP_CONF_DIR/httpfs-site.xml httpfs HTTPFS_CONF
 configure $HADOOP_CONF_DIR/kms-site.xml kms KMS_CONF
+configure $HADOOP_CONF_DIR/mapred-site.xml mapred MAPRED_CONF
 
 if [ "$MULTIHOMED_NETWORK" = "1" ]; then
     echo "Configuring for multihomed network"
