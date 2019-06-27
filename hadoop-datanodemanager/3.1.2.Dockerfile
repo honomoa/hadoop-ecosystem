64b7d8d5ce986d1227305e69437d96d9dbf7effd
Moa <honomoa@gmail.com>
2019-06-27 16:03:48 +0800

    Add datanodemanager

---
+++ hadoop-datanodemanager/3.1.2.Dockerfile
@@ -0,0 +1,14 @@
+FROM honomoa/hadoop-base:3.1.2
+
+HEALTHCHECK CMD netstat -nl | egrep "8042|9864" > /dev/null; if [ 0 != $? ]; then exit 1; fi;
+
+ENV HDFS_CONF_dfs_datanode_data_dir=file:///hadoop/dfs/data
+RUN mkdir -p /hadoop/dfs/data
+VOLUME /hadoop/dfs/data
+
+ADD run.sh /run.sh
+RUN chmod a+x /run.sh
+
+EXPOSE 9864 8042
+
+CMD ["/run.sh"]
---
+++ hadoop-datanodemanager/run.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+datadir=`echo $HDFS_CONF_dfs_datanode_data_dir | perl -pe 's#file://##'`
+if [ ! -d $datadir ]; then
+  echo "Datanode data directory not found: $datadir"
+  exit 2
+fi
+
+function wait_for_it()
+{
+    local serviceport=$1
+    local service=${serviceport%%:*}
+    local port=${serviceport#*:}
+    local retry_seconds=5
+    local max_try=100
+    let i=1
+
+    nc -z $service $port
+    result=$?
+
+    until [ $result -eq 0 ]; do
+      echo "[$i/$max_try] check for ${service}:${port}..."
+      echo "[$i/$max_try] ${service}:${port} is not available yet"
+      if (( $i == $max_try )); then
+        echo "[$i/$max_try] ${service}:${port} is still not available; giving up after ${max_try} tries. :/"
+        exit 1
+      fi
+      
+      echo "[$i/$max_try] try in ${retry_seconds}s once again ..."
+      let "i++"
+      sleep $retry_seconds
+
+      nc -z $service $port
+      result=$?
+    done
+    echo "[$i/$max_try] $service:${port} is available."
+}
+
+for i in ${SERVICE_PRECONDITION_DN[@]}
+do
+    wait_for_it ${i}
+done
+$HADOOP_HOME/bin/hdfs --config $HADOOP_CONF_DIR --daemon start datanode
+
+for i in ${SERVICE_PRECONDITION_NM[@]}
+do
+    wait_for_it ${i}
+done
+$HADOOP_HOME/bin/yarn --config $HADOOP_CONF_DIR nodemanager
